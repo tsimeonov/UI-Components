e5bf8272f4951eed08571489ad98f96da1874911
tsimeonov <tsimeonov12@gmail.com>
2025-03-30 16:41:40 +0200

    update js

--- Side Navigation Bar_01/app.js
+++ Side Navigation Bar_01/app.js
@@ -2,13 +2,17 @@ const sidebar = document.querySelector(".sidebar");
 const sidebarToggler = document.querySelector(".sidebar-toggler");
 const menuToggler = document.querySelector(".menu-toggler");
 
+const collapsedSidebarHeight = "56px";
+
 // Toggle sidebar's collapsed state
 sidebarToggler.addEventListener("click", () => {
   sidebar.classList.toggle("collapsed");
 });
 
 const toggleMenu = (isMenuActive) => {
-  sidebar.style.height = isMenuActive ? `${sidebar.scrollHeight}px` : ``;
+  sidebar.style.height = isMenuActive
+    ? `${sidebar.scrollHeight}px`
+    : collapsedSidebarHeight;
 };
 
 menuToggler.addEventListener("click", () => {
